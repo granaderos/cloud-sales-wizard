ce816274b00d6bb5aa83631ce75abe62812b9f53
Marejean-Perpinosa <marejean.perpinosa@ibm.com>
2019-07-08 15:31:32 +0800

    Wireframe.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var current_question = 1;
+var total_questions = 6;
+$(document).ready(function() {
+    $("#performaceSlider").slider();
+});
+
+function next() {
+    if(current_question == total_questions - 1) {
+        $("#btnNext").hide();
+        $("#btnSubmit").show();
+    } 
+
+    $("#question_" + current_question).hide();
+    ++current_question;
+    $("#question_" + current_question).show("slide", { direction: "right" }, 1000);
+}
+
+function submit() {
+    $("#divOutput").show();
+    $("#divRedhat").show();
+    $("#divGoogleCloud").show();
+    $("#questions_div").hide();
+}
